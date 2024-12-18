0a5fe3336463d0c6ba581edfdaa258e01c11464a
Monty <montydmontilla@gmail.com>
2024-12-17 23:22:38 -0500

    Add ability to chose between black and random colors for drawings

--- scripts.js
+++ scripts.js
@@ -1,21 +1,38 @@
 const container = document.querySelector(".container");
 const eraseGridButton = document.querySelector("#erase");
 
+const generateRandomRGB = () => {
+    const red = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${blue}, ${green})`;
+}
+const setRandomHighlight = (e) => {
+    const color = generateRandomRGB();
+    e.target.style.backgroundColor =  color;
+};
+
 const highlightSquare = (e) => {
     e.target.classList.add("highlight");
 }
 
 const eraseGrid = () => {
     container.innerHTML = "";
-    let size = parseInt(prompt('Enter one number for the width and height of your new Etch-a-Sketch:'));
+    let size = parseInt(prompt('Enter one number for pixel size of your new Etch-a-Sketch (Ex. Enter 16 for a 16X16 grid.):'));
     while (size === NaN
         || size < 1
         || size > 100
     ) size = parseInt(prompt('Please enter a whole number between 1 and 100:'));
-    createGrid(size);
+    let color = parseInt(prompt('Chose your line color. Enter 0 for black or 1 for random colors.'));
+    while( color == NaN
+        || color < 0
+        || color > 1
+    ) color = prompt('Please enter a valid selection. Pick either 0 (black) or 1 (random colors):');
+    createGrid(size, color);
 }
 
-const createGrid = (size) => {
+const createGrid = (size,color = 0) => {
     let newSquares = '';
     const widthAndHeight = 100 / size;
     for (let i = 0; i < size * size; i++) {
@@ -26,7 +43,8 @@ const createGrid = (size) => {
     gridSquares.forEach(square => {
         square.style.height = `${widthAndHeight}%`;
         square.style.width = `${widthAndHeight}%`;
-        square.addEventListener("mouseover", highlightSquare);
+        if (color === 0) square.addEventListener("mouseover", highlightSquare);
+        if (color === 1) square.addEventListener("mouseover", setRandomHighlight);
     })
 }
 
