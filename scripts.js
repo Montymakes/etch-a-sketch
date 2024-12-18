23e1346ded423faa10f80d2a7b70f6c8513018e9
Monty <montydmontilla@gmail.com>
2024-12-17 21:59:51 -0500

    Apply Etch-a-Sketch themed styles Add button to erase board When clicked, button calls eraseGrid function that erases the sketch prompts user for new board dimensions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
 const container = document.querySelector(".container");
+const eraseGridButton = document.querySelector("#erase");
 
 const highlightSquare = (e) => {
     e.target.classList.add("highlight");
+}
+
+const eraseGrid = () => {
+    container.innerHTML = "";
+    let size = parseInt(prompt('Enter one number for the width and height of your new Etch-a-Sketch:'));
+    while (size === NaN
+        || size < 1
+        || size > 100
+    ) size = parseInt(prompt('Please enter a whole number between 1 and 100:'));
+    createGrid(size);
 }
 
 const createGrid = (size) => {
@@ -13,11 +24,11 @@ const createGrid = (size) => {
     container.innerHTML = newSquares;
     const gridSquares = document.querySelectorAll(".square");
     gridSquares.forEach(square => {
-        square.style.height = `${widthAndHeight}vh`;
-        square.style.width = `${widthAndHeight}vw`;
+        square.style.height = `${widthAndHeight}%`;
+        square.style.width = `${widthAndHeight}%`;
         square.addEventListener("mouseover", highlightSquare);
     })
 }
 
-
 createGrid(16);
+eraseGridButton.addEventListener("click", eraseGrid);
